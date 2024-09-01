714549a89cea88b2c9531f7eb20167a3d9638945
Shashank Soni <shashanksoni815@gmail.com>
2024-09-01 23:11:30 +0530

    string

---
+++ string.js
@@ -0,0 +1,16 @@
+// Strings
+
+let msg = "  hello    ";
+console.log(msg.trim());
+// msg.trim(); Helps to delete unnecessary spaces.
+
+let password = prompt("set your pasword");
+let newPass = password.trim();
+console.log(newPass);
+
+// Uppercase and Lowercase letters
+let yourname = "Shashank Soni";
+console.log(yourname.toLowerCase());
+console.log(yourname.toUpperCase());
+
+// String methods with Arguments
