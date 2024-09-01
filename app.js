6a1f2a526effda4dfa7ea238d7bd5342f28a1b39
Shashank Soni <shashanksoni815@gmail.com>
2024-09-01 10:23:07 +0530

    Arithmetic Operaters

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+console.log("Shashank Soni");
+
+let a = 10;
+let b = 5;
+console.log("Sum is =", a+b);
+
+// Tamplate Literals ${}
+let pencilPrice = 10;
+let ereaserPrice = 5;
+console.log("The total price is :", pencilPrice+ereaserPrice, "Rs.");
+
+// `` Back Ticks
+console.log(`The total price is : ${pencilPrice+ereaserPrice} Rs.`)
+
+// Arithmetic Operaters
+// let a = 10;
+// let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// Unery Operaters
+console.log(a++); //10
+console.log(++a); //12
