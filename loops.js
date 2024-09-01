6ef5b802858b81acf03e267324e6f0c56ffb736e
Shashank Soni <shashanksoni815@gmail.com>
2024-09-02 00:23:31 +0530

    123

---
+++ loops.js
@@ -0,0 +1,20 @@
+// used to repeat a peace of code
+for (let i = 1; i <= 6; i++) {
+    console.log(i);
+    
+}
+
+// even numbers
+for (let i = 2; i <= 20; i+=2){
+    console.log(i);
+}
+
+// odd numbers 
+for (let i = 1; i <= 20; i+=2) {
+    console.log(i);   
+}
+
+// multiplication table
+// for() {
+//     console.log(i + "x" + )
+// }
