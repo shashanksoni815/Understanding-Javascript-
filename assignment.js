89ea072a958bd275db0a8ff8c1cf3d22991c688b
Shashank Soni <shashanksoni815@gmail.com>
2024-09-02 18:23:50 +0530

    123

---
+++ assignment.js
@@ -0,0 +1,34 @@
+// file attached with todo.html
+
+// Q1. Write a JS program to delete all occurrences of element ‘num = 2’ in a given array
+
+// let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+// let num = 2;
+
+// for (let i = 0; i<arr.length; i++) {
+//     if (arr[i] == num) {
+//         arr.splice();
+//     }else{
+//         console.log(arr[i]);
+//     }   
+// }
+
+// Q2.Write a JS program to find the no of digits in a number.
+
+// let number = 287152;
+
+// for (let i = 0; i < number.length; i++) {
+//     let count = 0;
+//     number.slice
+//     count+=1;
+// }
+
+// Print factorial of n
+
+let n = prompt();
+let fact = 1;
+
+for (let i = 1; i <= n ; i++) {
+    fact*=i;   
+}
+console.log(fact);
