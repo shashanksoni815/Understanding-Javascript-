55685fe746e46006e923950833febb9bda2734e3
Shashank Soni <shashanksoni815@gmail.com>
2024-09-03 13:48:20 +0530

    object

--- object Littrels/object.js
+++ object Littrels/object.js
@@ -6,3 +6,40 @@ const student = {
     marks: 93.5,
     city:"Indore"
 };
+
+// objects of objects
+
+const classInfo = {
+    aman: {
+        grade: "A",
+        city: "Delhi"
+    },
+    sheadha: {
+        grade: "A",
+        city: "Pune"
+    },
+    karan: {
+        grade: "A",
+        city: "Mumbai"
+    }
+};
+
+// Arrays of Objects
+
+const classInformation = [
+    {
+        name: "aman",
+        grade: "A",
+        city: "Delhi"
+    },
+    {
+        name: "shashank",
+        grade: "A",
+        city: "Pune"
+    },
+    {
+        name: "karan",
+        grade: "0",
+        city: "Mumbai"
+    }
+];
