9eecad46e11915ee19d35103a76da86fa45574ce
Shashank Soni <shashanksoni815@gmail.com>
2024-10-18 23:12:32 +0530

    data types

--- recalling_js/basic-1/03-DtypeConversion.js
+++ recalling_js/basic-1/03-DtypeConversion.js
@@ -11,3 +11,18 @@ let valueInNumber = Number(score)
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
