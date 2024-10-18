e12afafabfad2326409ff91975ab3748b52a5f8a
Shashank Soni <shashanksoni815@gmail.com>
2024-10-18 23:11:01 +0530

    data types

---
+++ recalling_js/basic-1/03-DtypeConversion.js
@@ -0,0 +1,13 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
