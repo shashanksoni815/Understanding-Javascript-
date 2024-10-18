2578cbc0e46bc9aded3eafe52d0203493acd4cb6
Shashank Soni <shashanksoni815@gmail.com>
2024-10-18 23:14:06 +0530

    data types

--- recalling_js/basic-1/03-DtypeConversion.js
+++ recalling_js/basic-1/03-DtypeConversion.js
@@ -26,3 +26,22 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
