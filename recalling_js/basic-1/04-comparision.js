5dd89bd810f4d41533e7bc44458358f5e8a2df31
Shashank Soni <shashanksoni815@gmail.com>
2024-10-20 18:37:43 +0530

    comparision

--- recalling_js/basic-1/04-comparision.js
+++ recalling_js/basic-1/04-comparision.js
@@ -1,8 +1,8 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
 
 
 // console.log("2" > 1);
