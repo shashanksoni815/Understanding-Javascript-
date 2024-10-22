428c579fca620ae6699da0deb55daa7793ae5f29
Shashank Soni <shashanksoni815@gmail.com>
2024-10-22 22:10:47 +0530

    data

--- recalling_js/basic-1/03-DtypeConversion.js
+++ recalling_js/basic-1/03-DtypeConversion.js
@@ -46,7 +46,7 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
+console.log("1" + 2);
 console.log(1 + "2");
 console.log("1" + 2 + 2);
 console.log(1 + 2 + "2");
