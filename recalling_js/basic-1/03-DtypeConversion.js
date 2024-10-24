ee34316571b234ab04d53f55b755c3b3b8eee707
Shashank Soni <shashanksoni815@gmail.com>
2024-10-24 17:11:26 +0530

    data

--- recalling_js/basic-1/03-DtypeConversion.js
+++ recalling_js/basic-1/03-DtypeConversion.js
@@ -25,7 +25,7 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
