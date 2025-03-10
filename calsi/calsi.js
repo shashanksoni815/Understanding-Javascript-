c1aef3b8e64efefdf3b11eae5b849139b58408a7
Shashank Soni <shashanksoni815@gmail.com>
2025-03-10 13:08:21 +0530

    calsi

--- calsi/calsi.js
+++ calsi/calsi.js
@@ -1,20 +1,38 @@
-let a = 2;
-let b = 5;
+let a = document.getElementById("num-A");
+let operator = document.querySelector(".operat");
+let b = document.querySelector("#num-B");
+let sub = document.querySelector("button");
+let num = a.value;
+// sub.addEventListener("click", function() {
+// switch (operator.ariaValueText) {
+//     case "+" : console.log(a + b);
+//         break;
+//     case "-" : console.log(a - b);
+//         break;
+//     case "*" : console.log(a * b);
+//         break;
+//     case "/" : console.log(a / b);
+//         break;
+//     case "%" : console.log(a % b);
+//         break;
+//     default: console.log("err");
+//         break;
+//     }
+//     console.log(num);
+// })
 
-let operator = "/";
+console.log(a.value)
 
 
-switch (operator) {
-    case "+" : console.log(a + b);
-        break;
-    case "-" : console.log(a - b);
-        break;
-    case "*" : console.log(a * b);
-        break;
-    case "/" : console.log(a / b);
-        break;
-    case "%" : console.log(a % b);
-        break;
-    default: console.log("err");
-        break;
-}
+
+
+
+
+
+// let a = 2;
+// let b = 5;
+
+// let operator = "/";
+
+
+// 
